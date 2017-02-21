201adb028a8cb18d4b2529ca643b2a3ece1fe97e
Hasan Tareque <hmtareque@gmail.com>
2017-02-21 15:05:11 +0000

    added dev tool

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
   return (
-    <div className="App">Redux</div>
+    <div className="App">
+      <h2>Counter: { counter }</h2>
+      <h2>isLogged: { isLogged }</h2>
+      </div>
   );
 }
 
