5c66896f967fe9b7b1d25829ea1f08048e394bc3
Hasan Tareque <hmtareque@gmail.com>
2017-02-21 13:42:06 +0000

    all reducer added to store

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore } from 'redux';
+import allReducers from './reducers';
+
+const store = createStore(allReducers);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
